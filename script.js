91813d84c3bf6bbbd64a37f0dc8ce3d1b49aad7b
BrianMampane <167504777+BrianMampane@users.noreply.github.com>
2024-10-11 02:17:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function displayCat() {
     // Create a new Image element for the cat
     var catImage = new Image();
     // Set the source (file path) for the cat image
-    catImage.src = 'chhii bye.webp'; // Assuming the cat image is named "cat.gif"
+    catImage.src = 'cat.gif'; // Assuming the cat image is named "cat.gif"
     // Set alternative text for the image (for accessibility)
     catImage.alt = 'Cat';
     // When the cat image is fully loaded, add it to the image container
